a0795a93c6b2885c4219120ade2e9cb707518456
lishen-razer <hljlishen@hotmail.com>
2020-04-04 18:09:10 +0800

    添加D2d动态链接库引用

--- Functions/Functions.cs
+++ Functions/Functions.cs
@@ -1,142 +1,143 @@
-﻿//using Microsoft.WindowsAPICodePack.DirectX.Direct2D1;
+﻿using Microsoft.WindowsAPICodePack.DirectX.Direct2D1;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace Utilities.Tools
 {
     public class Functions
     {
         public const float Interval = 0.0001f;
-        //public static ColorF GetColorFFromRgb(int r, int g, int b) => new ColorF(new ColorI(r, g, b));
+        public static ColorF GetColorFFromRgb(int r, int g, int b) => new ColorF(new ColorI(r, g, b));
 
         public static double DegreeToRadian(double angle) => Math.PI * angle / 180;//角度转弧度
 
         public static double RadianToDegree(double radian) => 180 * radian / Math.PI;//弧度转角度
 
-        //public static Rect ZoomRectangle(Rect rect, double percent)     //以rect的中心点为中心，将rect放缩percent的百分比
-        //{
-        //    var r = new Rect();
-        //    var location = new Point2F();
-        //    double offsetX, offsetY;
-        //    r.Width = (int)(rect.Width * percent);
-        //    r.Height = (int)(rect.Height * percent);
+        public static Rect ZoomRectangle(Rect rect, double percent)     //以rect的中心点为中心，将rect放缩percent的百分比
+        {
+            var r = new Rect();
+            var location = new Point2F();
+            double offsetX, offsetY;
+            r.Width = (int)(rect.Width * percent);
+            r.Height = (int)(rect.Height * percent);
 
-        //    if (percent < 1)    //缩小
-        //    {
-        //        offsetX = (double)(rect.Width - r.Width) / 2;
-        //        offsetY = (double)(rect.Height - r.Height) / 2;
-        //        location.X = rect.Left + (int)offsetX;
-        //        location.Y = rect.Top + (int)offsetY;
-        //    }
-        //    else   //放大
-        //    {
-        //        offsetX = (double)(r.Width - rect.Width) / 2;
-        //        offsetY = (double)(r.Height - rect.Height) / 2;
-        //        location.X = rect.Left - (int)offsetX;
-        //        location.Y = rect.Top - (int)offsetY;
-        //    }
+            if (percent < 1)    //缩小
+            {
+                offsetX = (double)(rect.Width - r.Width) / 2;
+                offsetY = (double)(rect.Height - r.Height) / 2;
+                location.X = rect.Left + (int)offsetX;
+                location.Y = rect.Top + (int)offsetY;
+            }
+            else   //放大
+            {
+                offsetX = (double)(r.Width - rect.Width) / 2;
+                offsetY = (double)(r.Height - rect.Height) / 2;
+                location.X = rect.Left - (int)offsetX;
+                location.Y = rect.Top - (int)offsetY;
+            }
 
-        //    r.Left = (int)location.X + rect.Left;
-        //    r.Top = (int)location.Y + rect.Top;
-        //    r.Right += (int)(location.X);
-        //    r.Bottom += (int)location.Y;
-        //    return r;
-        //}
+            r.Left = (int)location.X + rect.Left;
+            r.Top = (int)location.Y + rect.Top;
+            r.Right += (int)(location.X);
+            r.Bottom += (int)location.Y;
+            return r;
+        }
 
-        //public static Point2F FindCenterPosition(Rect rect)
-        //{
-        //    if (rect == null)
-        //        throw (new Exception("rectangle对象为空"));
-        //    var p = new Point2F
-        //    {
-        //        X = rect.Left + rect.Width / 2,
-        //        Y = rect.Top + rect.Height / 2
-        //    };
+        public static Point2F FindCenterPosition(Rect rect)
+        {
+            if (rect == null)
+                throw (new Exception("rectangle对象为空"));
+            var p = new Point2F
+            {
+                X = rect.Left + rect.Width / 2,
+                Y = rect.Top + rect.Height / 2
+            };
 
-        //    return p;
-        //}       //找矩形中心点
+            return p;
+        }       //找矩形中心点
 
-        //public static float AngleToNorth(Point2F center, Point2F p)    //两点连线与垂直线的夹角
-        //{
-        //    var distance = (float)DistanceBetween(center, p);
-        //    var x = p.X - center.X;
-        //    var angleR = (float)Math.Asin(x / distance);
-        //    var angle = RadianToDegree(angleR);
+        public static float AngleToNorth(Point2F center, Point2F p)    //两点连线与垂直线的夹角
+        {
+            var distance = (float)DistanceBetween(center, p);
+            var x = p.X - center.X;
+            var angleR = (float)Math.Asin(x / distance);
+            var angle = RadianToDegree(angleR);
 
-        //    //float y = center.Y - p.Y;
-        //    //double angleR1 = Math.Acos(y / distance);
-        //    //angleR1 = RadianToDegree(angleR1);
+            //float y = center.Y - p.Y;
+            //double angleR1 = Math.Acos(y / distance);
+            //angleR1 = RadianToDegree(angleR1);
 
-        //    if (p.X >= center.X && p.Y <= center.Y)   //第一象限
-        //    {
+            if (p.X >= center.X && p.Y <= center.Y)   //第一象限
+            {
 
-        //    }
-        //    else if (p.X >= center.X && p.Y > center.Y) //第二象限
-        //    {
-        //        angle = 180 - angle;
-        //    }
-        //    else if (p.X < center.X && p.Y > center.Y) //第三象限
-        //    {
-        //        angle = 180 + Math.Abs(angle);
-        //    }
-        //    else//第四象限
-        //    {
-        //        angle = 360 + angle;
-        //    }
+            }
+            else if (p.X >= center.X && p.Y > center.Y) //第二象限
+            {
+                angle = 180 - angle;
+            }
+            else if (p.X < center.X && p.Y > center.Y) //第三象限
+            {
+                angle = 180 + Math.Abs(angle);
+            }
+            else//第四象限
+            {
+                angle = 360 + angle;
+            }
 
-        //    return (float)angle;
-        //}
+            return (float)angle;
+        }
 
-        //public static double DistanceBetween(Point2F p1, Point2F p2)
-        //{
-        //    var a = Math.Pow((double)p1.X - p2.X, 2);
-        //    var b = Math.Pow((double)p1.Y - p2.Y, 2);
-        //    var c = Math.Sqrt(a + b);
+        public static double DistanceBetween(Point2F p1, Point2F p2)
+        {
+            var a = Math.Pow((double)p1.X - p2.X, 2);
+            var b = Math.Pow((double)p1.Y - p2.Y, 2);
+            var c = Math.Sqrt(a + b);
 
-        //    return c;
-        //}   //两点间的距离
+            return c;
+        }   //两点间的距离
 
-        //public static Rect MoveRect(Rect r, Point2F p) //将r横向移动p.x, 纵向移动p.y
-        //{
-        //    var ret = r;
-        //    ret.Left += (int)p.X;
-        //    ret.Right += (int)p.X;
-        //    ret.Top += (int)p.Y;
-        //    ret.Bottom += (int)p.Y;
+        public static Rect MoveRect(Rect r, Point2F p) //将r横向移动p.x, 纵向移动p.y
+        {
+            var ret = r;
+            ret.Left += (int)p.X;
+            ret.Right += (int)p.X;
+            ret.Top += (int)p.Y;
+            ret.Bottom += (int)p.Y;
 
-        //    return ret;
-        //}
+            return ret;
+        }
 
-        //public static RectF MoveRectToPoint(RectF rect, Point2F point)
-        //{
-        //    float width = rect.Width;
-        //    float height = rect.Height;
+        public static RectF MoveRectToPoint(RectF rect, Point2F point)
+        {
+            float width = rect.Width;
+            float height = rect.Height;
 
-        //    rect.Left = point.X;
-        //    rect.Top = point.Y;
-        //    rect.Right = rect.Left + width;
-        //    rect.Bottom = rect.Top + height;
+            rect.Left = point.X;
+            rect.Top = point.Y;
+            rect.Right = rect.Left + width;
+            rect.Bottom = rect.Top + height;
 
-        //    return rect;
-        //}
+            return rect;
+        }
 
-        //public static Point2F PointToPoint2F(Point p) => new Point2F(p.X, p.Y);
+        public static Point2F PointToPoint2F(Point p) => new Point2F(p.X, p.Y);
 
-        //public static Point Point2FToPoint(Point2F p) => new Point((int)p.X, (int)p.Y);
+        public static Point Point2FToPoint(Point2F p) => new Point((int)p.X, (int)p.Y);
 
-        //public static Rectangle D2DRectToGdiRectangle(Rect r)
-        //{
-        //    var ret = new Rectangle();
+        public static Rectangle D2DRectToGdiRectangle(Rect r)
+        {
+            var ret = new Rectangle();
 
-        //    //左上角位置
-        //    var p = new Point(r.Left, r.Top);
-        //    ret.Location = p;
+            //左上角位置
+            var p = new Point(r.Left, r.Top);
+            ret.Location = p;
 
-        //    ret.Width = r.Right - r.Left;
-        //    ret.Height = r.Bottom - r.Top;
+            ret.Width = r.Right - r.Left;
+            ret.Height = r.Bottom - r.Top;
 
-        //    return ret;
-        //}
+            return ret;
+        }
 
         public static float FindSmallArcBeginAngle(float a, float b)
         {
